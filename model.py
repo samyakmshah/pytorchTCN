93e0528e0852f025e5a7715469a875b4b647f1c5
Samyak Shah <samyakshah@biggie.westell.com>
2019-05-04 01:14:50 -0400

    Completed model for MNIST

---
+++ model.py
@@ -0,0 +1,17 @@
+import torch.nn.Functional as F
+from torch import nn
+from tcn import TCN_base
+
+class TCN(nn.Module):
+	def __init__(self, in_n, out_n, ch_n, kernel_size, dropout):
+		super(TCN, self).__init__()
+		self.tcn = TCN_base(in_n, ch_n, kernel_size, dropout)
+		self.linear = nn.linear(ch_n[-1], out_n)
+
+	def forward(self, x):
+		y = self.tcn(x) # (Batch, Channels, Length)
+		out = self.linear(y[:, :, -1]) # take the last output
+		return F.log_softmax(out, dim = 1)
+
+
+
